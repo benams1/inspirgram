e32f6fd9dfd60c3bcc50411fe760797ebaa1ae8a
benamsalem <42165002+benams1@users.noreply.github.com>
2020-02-23 13:44:08 +0200

    cross origin resource sharing changes

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const authMiddleware = require('./middlewares/authMiddleware');
 const { SentenceRouter, OrdersRouter, UsersRouter ,PaypalRouter} =require('./routers');
-
+const { TOKEN_NAME } = require('./utilities/consts');
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
+app.use(cors());
 app.use(
     (req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept',);
+            `Origin, X-Requested-With, Content-Type, ${TOKEN_NAME}, Accept`,);
         res.set('Content-Type', 'application/json');
         next();
     });
